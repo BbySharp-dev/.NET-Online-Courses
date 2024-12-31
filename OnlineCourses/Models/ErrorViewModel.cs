ba826fadbfffb3d1e36afe05640e6495f977b69d
BbySharp-dev <doanminhtruong53.dev@gmail.com>
2024-12-31 11:43:05 +0700

    Project theme

--- OnlineCourses/Models/ErrorViewModel.cs
+++ OnlineCourses/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace OnlineCourses.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
